2c367e408dbe1b6cc4fdd70edcc9b66421962e64
Morgan Atwood <atwood.morganj@gmail.com>
2020-06-03 16:55:40 -0700

    refactor-organize directories

--- public/scripts.js
+++ public/scripts.js
@@ -1,11 +1,5 @@
-
 const socket = io('http://localhost:9003');
 
-
-socket.on('messageFromServer', (dataFromServer) => {
-  console.log(dataFromServer);
-  socket.emit('messageToServer', { data: "data from the Client**" })
-})
 
 socket.on('nsList', (nsData) => {
   console.log('The list of namespaces has arrived')
@@ -22,42 +16,6 @@ socket.on('nsList', (nsData) => {
       console.log(`${nsEndpoint} I should go to now`)
     })
   })
-
-  const nsSocket = io('http://localhost:9003/wiki')
-  nsSocket.on('nsRoomLoad', (nsRooms) => {
-    let roomList = document.querySelector('.room-list');
-    roomList.innerHTML = "";
-    nsRooms.forEach(room => {
-      let glyph;
-      if (room.privateRoom) {
-        glpyh = 'lock'
-      } else {
-        glpyh = 'globe'
-      }
-      roomList.innerHTML += `<li class="room"><span class="glyphicon glyphicon-${glpyh}"></span>${room.roomTitle}</li>`
-    })
-
-    let roomNodes = document.getElementsByClassName('room')
-    Array.from(roomNodes).forEach(elem => {
-      elem.addEventListener('click', (e) => {
-        console.log("Someone clicked on ", e.target.innerText)
-      })
-    })
-  })
-})
-
-document.querySelector('#message-form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  console.log('Form submitted');
-
-  const newMessage = document.querySelector('#user-message').value;
-
-  socket.emit('newMessageToServer', { text: newMessage })
-
-})
-
-socket.on('messageToClients', (msg) => {
-  console.log(msg);
-  document.querySelector('#messages').innerHTML += `<li>${msg.text}</li>`
+  joinNs('/wiki')
 })
 
